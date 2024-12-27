eba86aec7c53899c68f3c277d199cade796fb837
Jorgos Papapostolu <RadioactiveRalph@pm.me>
2024-12-27 21:50:37 +0000

    Register Page inkl. Passworteingabe + Prüfung ob die übereinstimmen -> Noch keine DB Connection dahinter

--- client/src/app/pages/register/register.component.ts
+++ client/src/app/pages/register/register.component.ts
@@ -1,24 +1,51 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ɵɵdeferHydrateOnViewport } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css'],
+  imports: [CommonModule, FormsModule]
 })
 export class RegisterComponent {
-   constructor(private router: Router) {}
+  email: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+  errorMessage: string | null = null;
+
+  constructor(private router: Router) {}
+
+  register(): void {
+    if (this.password !== this.confirmPassword) {
+      console.log("Passwort stimmt nicht überein.")
+      this.errorMessage = 'Die Passwörter stimmen nicht überein.';
+    } else if (!this.email || !this.password || !this.confirmPassword) {
+      console.log("Alle Felder ausfüllen")
+      this.errorMessage = 'Bitte fülle alle Felder aus.';
+    } else if (this.password === this.confirmPassword) {
+      console.log('Registrierung erfolgreich:', {
+        email: this.email,
+        password: this.password
+      });
   
-    navigateToLogin() {
+      this.errorMessage = null;
+  
       this.router.navigate(['/login']);
     }
-  
-    navigateToRegister() {
-      this.router.navigate(['/register']);
-    }
-  
-    navigateToMainpage() {
-      this.router.navigate([''])
-    }
+  }
+
+  navigateToLogin(): void {
+    this.router.navigate(['/login']);
+  }
+
+  navigateToMainpage(): void {
+    this.router.navigate(['/']);
+  }
+
+  navigateToRegister() {
+    this.router.navigate(['/register']);
+  }
 }
--- client/src/main.ts
+++ client/src/main.ts
@@ -2,9 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { FormsModule } from '@angular/forms';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes)
+    provideRouter(routes),
+    FormsModule
   ]
 }).catch(err => console.error(err));
