9d50a43ec3bce3dc132fad8755c2cc2e4c736923
Jorgos Papapostolu <RadioactiveRalph@pm.me>
2024-12-28 11:11:56 +0000

    Set up PostgreSQL connection for NestJS backend via Docker - Configured Docker Compose with backend and PostgreSQL services - Verified database connection using TypeORM in NestJS - Added psql client for debugging database connections

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Umgebungsvariablen global zugänglich machen
+    }),               // Damit kann ich dann process.env.POSTRES_XXXX verwenden
+    TypeOrmModule.forRoot({ // Das baut die Verbindung zur DB auf
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST, 
+      port: parseInt(process.env.POSTGRES_PORT, 10),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      autoLoadEntities: true,
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
